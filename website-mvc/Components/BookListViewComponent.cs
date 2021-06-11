5e763b177b743e7b42e5b21faadb8f193bcecc98
clementia1 <grininma0@gmail.com>
2021-06-11 10:12:50 +0300

    BookList viewcomponent

---
+++ website-mvc/Components/BookListViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace website_mvc.Components
+{
+    public class BookListViewComponent : ViewComponent
+    {
+        List<string> books;
+        public BookListViewComponent()
+        {
+            books = new List<string>
+            {
+                "C# 8.0 Pocket Reference: Instant Help for C# 8.0 Programmers",
+                "Pro C# 7: With .NET and .NET Core",
+                "Agile Principles, Patterns, and Practices in C#",
+                "Concurrency in C# Cookbook: Asynchronous, Parallel, and Multithreaded Programming"
+            };
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(books);
+        }
+    }
+}
